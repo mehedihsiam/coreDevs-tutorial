39721a8300e284e18e9153617e9bd329cbda028f
Mehedi Hasan Siam <mehedisiam0022@gmail.com>
2021-12-13 13:52:09 +0600

    update

--- src/Pages/UsersPages/Payment/Payment.js
+++ src/Pages/UsersPages/Payment/Payment.js
@@ -1,21 +1,28 @@
-import { Button } from '@mui/material';
-import React, { useState } from 'react';
-import Paypal from './Paypal/Paypal';
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import React from 'react';
+import PaypalExpressBtn from 'react-paypal-express-checkout';
 
 const Payment = () => {
-    const [checkout, setCheckout] = useState(false)
+    const onSuccess = (payment) => {
+        console.log("The payment was succeeded!", payment);
+    }
+    const onCancel = (data) => {
+        console.log('The payment was cancelled!', data);
+    }
+    const onError = (err) => {
+        console.log("Error!", err);
+    }
+    let env = 'sandbox';
+    let currency = 'USD';
+    let total = 1;
+
+    const client = {
+        sandbox: 'ASSY6pn30b_3-9l0rNCXMEqxUiE7xXD4cAJ5hGHjQ6NoEmC13yRF4FJR0NF75x4vj9dHDvujheXWBrCH',
+        production: 'YOUR-PRODUCTION-APP-ID',
+    }
+
     return (
         <div>
-            {
-                checkout ?
-                    <Paypal />
-                    :
-                    <Button onClick={() => setCheckout(true)}>Checkout</Button>
-            }
-            <PayPalScriptProvider options={{ "client-id": "test" }}>
-                <PayPalButtons style={{ layout: "horizontal" }} />
-            </PayPalScriptProvider>
+            <PaypalExpressBtn env={env} client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
         </div>
     );
 };
